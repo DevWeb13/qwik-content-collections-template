6d66891dd9dbe66566b10696d1e3ee4519c867fe
DevWeb13 <giuliano.loic@sfr.fr>
2024-10-16 11:26:34 +0200

    tttt

--- content-collections.ts
+++ content-collections.ts
@@ -1,7 +1,7 @@
 // content-collections.ts
 
 import { defineCollection, defineConfig } from '@content-collections/core';
-import { compileMarkdown } from '@content-collections/markdown';
+
 const posts = defineCollection({
   name: 'posts',
   directory: 'src/posts',
@@ -9,15 +9,7 @@ const posts = defineCollection({
   schema: (z) => ({
     title: z.string(),
     summary: z.string(),
-    content: z.string(),
   }),
-  transform: async (document, context) => {
-    const html = await compileMarkdown(context, document);
-    return {
-      ...document,
-      html,
-    };
-  },
 });
 
 export default defineConfig({
