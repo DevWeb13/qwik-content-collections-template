6ef37be6aedecbb62dd1be1066b4fa72ce75188b
DevWeb13 <giuliano.loic@sfr.fr>
2024-10-16 11:16:29 +0200

    test ??

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,13 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),  qwikPwa({}), contentCollections()],
+    resolve: {
+      alias: {
+        '~': '/src',
+        'content-collections': '/.content-collections/generated'
+      }
+    },
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
